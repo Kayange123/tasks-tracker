5598e73056e492ca34be634a433536640f2d4074
Ayubu Kayange <kayangejr3@gmail.com>
2023-12-06 15:39:08 +0300

    added new features to organization dashboard

--- app/(platform)/(dashboard)/_components/NavItem.tsx
+++ app/(platform)/(dashboard)/_components/NavItem.tsx
@@ -1,6 +1,7 @@
 import { AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { cn } from "@/lib/utils";
-import React from "react";
+import { Layout } from "lucide-react";
+import Image from "next/image";
 
 export type TOrganization = {
   id: string;
@@ -21,13 +22,33 @@ const NavItem = ({
   onExpand,
   organization,
 }: NavItemProps) => {
+  const routes = [
+    {
+      label: "Boards",
+      icon: <Layout className="w-4 h-4" />,
+      href: `/organization/${organization.id}`,
+    },
+  ];
   return (
     <AccordionItem value={organization.id} className="border-none">
       <AccordionTrigger
         onClick={() => onExpand(organization.id)}
-        className={cn("flex items-center gap-x-2 p-1.5 rounded-md text-start no-underline hover:no-underline text-neutral-700 hover:bg-neutral-500/10",isActive && !isExpanded && "bg-sky-500/10 text-sky-700")}
+        className={cn(
+          "flex items-center gap-x-2 p-1.5 rounded-md text-start no-underline hover:no-underline text-neutral-700 hover:bg-neutral-500/10",
+          isActive && !isExpanded && "bg-sky-500/10 text-sky-700"
+        )}
       >
-        
+        <div className="flex items-center gap-x-2">
+          <div className="w-7 h-7 relative">
+            <Image
+              src={organization.imageUrl}
+              alt="organization image"
+              fill
+              className="rounded-sm object-cover"
+            />
+          </div>
+          <span className="text-sm font-medium">{organization?.name}</span>
+        </div>
       </AccordionTrigger>
     </AccordionItem>
   );
