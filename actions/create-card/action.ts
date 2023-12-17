e92a0b21ce8dbc05b40e370910559dc6121748f6
Ayubu Kayange <kayangejr3@gmail.com>
2023-12-17 22:38:00 +0300

    added server action to create card

---
+++ actions/create-card/action.ts
@@ -0,0 +1,61 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { InputType, ReturnType } from "./types";
+import { db } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { createActions } from "@/lib/createActions";
+import { CreateCard } from "./schema";
+
+export const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+  const { title, boardId, listId } = data;
+  let card;
+  try {
+    const list = await db.list.findUnique({
+      where: {
+        id: listId,
+        board: {
+          orgId,
+        },
+      },
+    });
+    if (!list)
+      return {
+        error: "List Not Found",
+      };
+
+    //Find the last card in the order
+    const lastCard = await db.card.findFirst({
+      where: { listId },
+      orderBy: { order: "desc" },
+      select: { order: true },
+    });
+
+    //Prepare the order of next card
+    const newOrder: number = lastCard ? lastCard.order + 1 : 1;
+
+    //Create the card
+    const card = await db.card.create({
+      data: {
+        title,
+        listId,
+        order: newOrder,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to create card",
+    };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: card };
+};
+
+export const createCard = createActions(CreateCard, handler);
---
+++ actions/create-card/schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const CreateCard = z.object({
+  title: z
+    .string({
+      required_error: "Title is required",
+      invalid_type_error: "Title is invalid",
+    })
+    .min(3, {
+      message: "Title should be more than 3 characters",
+    }),
+  boardId: z.string(),
+  listId: z.string(),
+});
